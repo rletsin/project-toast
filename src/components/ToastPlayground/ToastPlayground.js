fb6bad5bc67e32805ad163eef8863d4afeb4ca34
Roman Letsin <rletsin25@gmail.com>
2024-06-16 12:43:05 +0300

    Exercise 3

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -2,14 +2,34 @@ import React from 'react';
 
 import Button from '../Button';
 import styles from './ToastPlayground.module.css';
-import Toast from '../Toast/Toast';
+import ToastShelf from '../ToastShelf/ToastShelf';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
-  const [selectedToast, setSelectedToast] = React.useState(VARIANT_OPTIONS[0]);
+  const [selectedVariant, setSelectedVariant] = React.useState(VARIANT_OPTIONS[0]);
   const [message, setMessage] = React.useState('');
-  const [showToast, setShowToast] = React.useState(false);
+  const [toastStack, setToastStack] = React.useState([]);
+
+  function addNewToast() {
+    setToastStack([...toastStack, {
+      message,
+      variant: selectedVariant,
+      id: crypto.randomUUID(),
+    }]);
+    setMessage('');
+    setSelectedVariant('notice');
+  }
+
+  function closeToast(id) {
+    const index = toastStack.findIndex(t => t.id === id);
+    console.log(`Closing toast with id ${id} at index ${index}`);
+    if (index !== -1) {
+      const updatedArray = [...toastStack];
+      updatedArray.splice(index, 1)
+      setToastStack(updatedArray);
+    }
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -18,12 +38,15 @@ function ToastPlayground() {
         <h1>Toast Playground</h1>
       </header>
       {
-        showToast && <Toast message={message}
-          variant={selectedToast}
-          onClose={() => setShowToast(false)}
-        />
+        toastStack.length > 0 && <ToastShelf toasts={toastStack} onCloseToast={closeToast} />
       }
-      <div className={styles.controlsWrapper}>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault()
+          addNewToast()
+        }}
+        className={styles.controlsWrapper}
+      >
         <div className={styles.row}>
           <label
             htmlFor="message"
@@ -55,9 +78,9 @@ function ToastPlayground() {
                   type="radio"
                   name="variant"
                   value={variant}
-                  checked={variant === selectedToast}
+                  checked={variant === selectedVariant}
                   onChange={event => {
-                    setSelectedToast(event.target.value);
+                    setSelectedVariant(event.target.value);
                   }}
                 />
                 {variant}
@@ -71,10 +94,10 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <Button onClick={() => setShowToast(true)}>Pop Toast!</Button>
+            <Button>Pop Toast!</Button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -3,15 +3,14 @@ import React from 'react';
 import Toast from '../Toast';
 import styles from './ToastShelf.module.css';
 
-function ToastShelf() {
+function ToastShelf({ toasts, onCloseToast }) {
   return (
     <ol className={styles.wrapper}>
-      <li className={styles.toastWrapper}>
-        <Toast variant="notice">Example notice toast</Toast>
-      </li>
-      <li className={styles.toastWrapper}>
-        <Toast variant="error">Example error toast</Toast>
-      </li>
+      {toasts.map((toast) => (
+        <li key={toast.id} className={styles.toastWrapper}>
+          <Toast variant={toast.variant} onClose={() => onCloseToast(toast.id)} message={toast.message} />
+        </li>
+      ))}
     </ol>
   );
 }
